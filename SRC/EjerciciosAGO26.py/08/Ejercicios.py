54b17946f186626435026d72529c6e61b5313614
Valentina Aristizabal Escobar <valentina.aristizabale@upb.edu.co>
2025-08-26 17:43:06 -0500

    EjerciciosClase

--- SRC/EjerciciosAGO26.py/08/Ejercicios.py
+++ SRC/EjerciciosAGO26.py/08/Ejercicios.py
@@ -38,7 +38,24 @@ if edad >= 0:
 else:
     print("La edad ingresada no es válida.")
 
-    
+## Ejercicio 4
 
-
-
+print("Ingrese la zona de envío. Elija un número")
+zona = int(input("1. Norteamérica\n2. Centroamérica\n3. Suramérica\n4. Europa\n5. Asia\nElija un número: "))
+if zona > 0 and zona < 6:
+    peso = int(input("Ingrese el peso del paquete en gramos: "))
+    if peso <= 5000:
+        if zona == 1:
+            total = peso * 11
+    elif zona == 2:
+        total = peso * 10
+    elif zona == 3:
+        total = peso * 12
+    elif zona == 4:
+        total = peso * 24
+    elif zona == 5:
+        total = peso * 27
+        print(f"El envío de su paquete cuesta ${total}. ") 
+        print("No se puede enviar paquetes de más de 5kg")
+    else:
+        print("La zona ingresada no existe. ")
