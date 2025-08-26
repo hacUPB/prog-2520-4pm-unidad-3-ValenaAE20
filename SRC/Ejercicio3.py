639cbbd43a038fdd2da55e6650f9e4586f105539
Valentina Aristizabal Escobar <valentina.aristizabale@upb.edu.co>
2025-08-26 16:57:37 -0500

    Ejercicios

--- SRC/Ejercicio3.py
+++ SRC/Ejercicio3.py
@@ -1,6 +1,44 @@
+## Ejercicio 1
+
 print(10 > 5)
 print("Hola" != "Mundo")
 print(3.14 <= 4.5)
 nombre = "Juan"
 print(nombre == "Juan")
+
+## Ejercicio 2
+
+envio = 0
+compra = int(input("Ingrese el valor de la compra>> "))
+if compra < 100000:
+	envio = 9000
+total = compra + envio
+print(f"El total de la compra es {total}")
+
+## Ejercicio 3
+
+edad = int(input("Por favor, ingresa tu edad: "))
+
+if edad >= 0:
+    if edad <= 5:
+        etapa = "Primera Infancia"
+    elif edad <= 11:
+        etapa = "Infancia"
+    elif edad <= 26:
+        if edad <= 18:
+            etapa = "Adolescencia"
+        if edad >= 14:
+            etapa = "Juventud"
+    elif edad <= 59:
+        etapa = "Adultez"
+    else:
+        etapa = "Persona Mayor (Envejecimiento y Vejez)"
+    
+    print(f"Tu etapa del ciclo de vida es: {etapa}")
+else:
+    print("La edad ingresada no es válida.")
+
+    
+
+
 
